ff4b6325b8f6509d0e38b99d6b1347175632c1de
Callum-James Smith <techdragon@tuta.io>
2019-02-16 21:19:17 +0000

    improved design

--- landing.php
+++ landing.php
@@ -76,148 +76,146 @@
     </header>
 
     <div class="container">
-
-        <div class="box-1"> <!-- TODO Box -->
+        <div class="container-wrapper">
             <h3 class="headerTitle">To Do</h3>
             <hr>
-
-            <?php 
-            /**
-             * This part checks to see if the user has any projects, if not they are forced to make one.
-             */
-                require 'db.php';
-
-                $ProjectSQL = "SELECT project_name FROM projects WHERE user_id='".$_SESSION['id']."'";
-                $Project_Name = mysqli_query($connection, $ProjectSQL);
-                $ProjectNameRows = mysqli_num_rows($Project_Name);
-
-                if ($ProjectNameRows <= 0) // If the user does not have any projects, they are forced to make one
-                {
-                    echo(
-                        '<form action="newProject.inc.php" method="post">
-                            <h3>Please make a new project</h3>
-                            <input type="text" name="project-name">
-                            <button type="submit" name="add-project">Add Project</button>
-                        </form>'
-                    );
-                    exit();
-                }
-
-                if (isset($_POST['add-item'])) 
+            <div class="box-1"> <!-- TODO Box -->
+                <?php 
                 /**
-                 * Checks to see if the user has selected the option to add a task.
-                 * if so, then it prints out a form asking the user to fill in details about it.
-                 * 
-                 * Vars:
-                 *  $TopForm = This contains the top part of the form including the <select> element.
-                 *  $RestOfForm = This contains the rest of the form (from the project name list down).
+                 * This part checks to see if the user has any projects, if not they are forced to make one.
                  */
-                {
-                    
-                    $TopForm = '<form action="addItem.inc.php" method="post">';
+                    require 'db.php';
 
-                    $RestOfForm = 
-                            '
-                                Title<input type="text" name="title" placeholder="Title..."><br/>
-                                Description<input type="text" name="task-desc" placeholder="Description..."><br/>
-                                Date<input type="date" name="task-date"><br/>
-                                Time<input type="time" name="task-time"><br/>
-                            <select name="task-state">
-                                <option name="To Do">To Do</option>
-                                <option name="In Progress">In Progress</option>
-                                <option name="Completed">Completed</option>
-                            </select>
-                            <select name="task-priority">
-                                <option name="high">High</option>
-                                <option name="medium">Medium</option>
-                                <option name="low">Low</option>
-                            </select>
-                            <button type="submit" name="add-task">Add Task</button>
-                        </form>';
-                    $ListOfProjects = mysqli_query($connection, "SELECT * FROM projects"); // The query that selects all of the data from table `projects`.
+                    $ProjectSQL = "SELECT project_name FROM projects WHERE user_id='".$_SESSION['id']."'";
+                    $Project_Name = mysqli_query($connection, $ProjectSQL);
+                    $ProjectNameRows = mysqli_num_rows($Project_Name);
 
-                    $row = mysqli_fetch_array($ListOfProjects); // This variable stores all of the data performed from the query above, into a nice little array.
-
-                    $project_names[] = $row['project_name']; // Store a new variable that only stores values in the column 'project_name'.
-
-                    echo($TopForm); // Prints out the top of the form
-                    echo($RestOfForm); // prints out the rest if the form.
-                }
-                
-                if (empty($_GET['projects'])) {
-                    $ProjectID = mysqli_query($connection, "SELECT * FROM projects WHERE user_id='".$_SESSION['id']."'");
-                }else {
-                    $ProjectID = mysqli_query($connection, "SELECT * FROM projects WHERE project_name='".$_GET['projects']."'");
-                }
-                $Project_ID = mysqli_fetch_assoc($ProjectID); // This variable stores all of the data performed from the query above, into a nice little array.
-                $ID = intval($Project_ID['projectID']); // THIS IS THE ID!
-                echo($ID);
-                $sql = "SELECT task_title,task_date,task_time,task_state,task_priority,task_desc FROM tasks WHERE projectID=".intval($ID)." ORDER BY tasks.task_date ASC";  
-                $result = $connection-> query($sql);
-
-                if ($result-> num_rows <= 0) {
-                    echo('<p>You Do Not Have Any Tasks</p>');
-                }
-                if ($result-> num_rows > 0) {
-                    while ($row = $result-> fetch_assoc()) // While there is data in the table
+                    if ($ProjectNameRows <= 0) // If the user does not have any projects, they are forced to make one
                     {
-                        $task_title = $row['task_title'];
-                        $task_priority = $row['task_priority'];
-                        $task_desc = $row['task_desc'];
-                        $task_date = $row['task_date'];
-                        $task_time = $row['task_time'];
-                        echo('
-                        <div class="item">
-
-                        <div class="title-wrapper">
-                
-                            <div class="task-title">
-                                <h1 class="taskTitle">'.$task_title.'</h1>
-                            </div>
-                
-                            <div class="task-priority">
-                                <p class="task-priority">'.$task_priority.'</p>
-                            </div>
-                
-                        </div>
-                        <hr class="taskTitle">
-                
-                        <div class="task-desc">
-                            <p class="task-desc">'.$task_desc.'</p>
-                        </div>
-                
-                        <div class="task-date-time">
-                            <p class="task-date-time">'.$task_date.' '.$task_time.'</p>
-                        </div>
-                
-                    </div>');
+                        echo(
+                            '<form action="newProject.inc.php" method="post">
+                                <h3>Please make a new project</h3>
+                                <input type="text" name="project-name">
+                                <button type="submit" name="add-project">Add Project</button>
+                            </form>'
+                        );
+                        exit();
                     }
-                }
 
-            ?>
+                    if (isset($_POST['add-item'])) 
+                    /**
+                     * Checks to see if the user has selected the option to add a task.
+                     * if so, then it prints out a form asking the user to fill in details about it.
+                     * 
+                     * Vars:
+                     *  $TopForm = This contains the top part of the form including the <select> element.
+                     *  $RestOfForm = This contains the rest of the form (from the project name list down).
+                     */
+                    {
+                        
+                        $TopForm = '<form action="addItem.inc.php" method="post">';
 
-            <div class="addItem">
+                        $RestOfForm = 
+                                '
+                                    Title<input type="text" name="title" placeholder="Title..."><br/>
+                                    Description<input type="text" name="task-desc" placeholder="Description..."><br/>
+                                    Date<input type="date" name="task-date"><br/>
+                                    Time<input type="time" name="task-time"><br/>
+                                <select name="task-state">
+                                    <option name="To Do">To Do</option>
+                                    <option name="In Progress">In Progress</option>
+                                    <option name="Completed">Completed</option>
+                                </select>
+                                <select name="task-priority">
+                                    <option name="high">High</option>
+                                    <option name="medium">Medium</option>
+                                    <option name="low">Low</option>
+                                </select>
+                                <button type="submit" name="add-task">Add Task</button>
+                            </form>';
+                        $ListOfProjects = mysqli_query($connection, "SELECT * FROM projects"); // The query that selects all of the data from table `projects`.
 
-                <form action="landing.php" method="post">
-                    <button class="add-item-button" type="submit" name="add-item"><i class="far fa-plus-square"></i></button>
-                </form>
+                        $row = mysqli_fetch_array($ListOfProjects); // This variable stores all of the data performed from the query above, into a nice little array.
 
+                        $project_names[] = $row['project_name']; // Store a new variable that only stores values in the column 'project_name'.
+
+                        echo($TopForm); // Prints out the top of the form
+                        echo($RestOfForm); // prints out the rest if the form.
+                    }
+                    
+                    if (empty($_GET['projects'])) {
+                        $ProjectID = mysqli_query($connection, "SELECT * FROM projects WHERE user_id='".$_SESSION['id']."'");
+                    }else {
+                        $ProjectID = mysqli_query($connection, "SELECT * FROM projects WHERE project_name='".$_GET['projects']."'");
+                    }
+                    $Project_ID = mysqli_fetch_assoc($ProjectID); // This variable stores all of the data performed from the query above, into a nice little array.
+                    $ID = intval($Project_ID['projectID']); // THIS IS THE ID!
+                    $sql = "SELECT task_title,task_date,task_time,task_state,task_priority,task_desc FROM tasks WHERE projectID=".intval($ID)." ORDER BY tasks.task_date ASC";  
+                    $result = $connection-> query($sql);
+
+                    if ($result-> num_rows <= 0) {
+                        echo('<p>You Do Not Have Any Tasks</p>');
+                    }
+                    if ($result-> num_rows > 0) {
+                        while ($row = $result-> fetch_assoc()) // While there is data in the table
+                        {
+                            $task_title = $row['task_title'];
+                            $task_priority = $row['task_priority'];
+                            $task_desc = $row['task_desc'];
+                            $task_date = $row['task_date'];
+                            $task_time = $row['task_time'];
+                            echo('
+                            <div class="item">
+
+                            <div class="title-wrapper">
+                    
+                                <div class="task-title">
+                                    <h1 class="taskTitle">'.$task_title.'</h1>
+                                </div>
+                    
+                                <div class="task-priority">
+                                    <p class="task-priority">'.$task_priority.'</p>
+                                </div>
+                    
+                            </div>
+                            <hr class="taskTitle">
+                    
+                            <div class="task-desc">
+                                <p class="task-desc">'.$task_desc.'</p>
+                            </div>
+                    
+                            <div class="task-date-time">
+                                <p class="task-date-time">'.$task_date.' '.$task_time.'</p>
+                            </div>
+                    
+                        </div>');
+                        }
+                    }
+
+                ?>
+			</div>
+				<div class="addItem">
+
+					<form action="landing.php" method="post">
+						<button class="add-item-button" type="submit" name="add-item"><i class="far fa-plus-square"></i></button>
+					</form>
+
+				</div>
             </div>
-
-        </div>
-
-        <div class="box-2"> <!-- IN PROGRESS Box -->
+        <div class="container-wrapper">
             <h3 class="headerTitle">In Progress</h3>
             <hr>
+            <div class="box-2"> <!-- IN PROGRESS Box -->
 
+            </div>
         </div>
 
-        <div class="box-3"> <!-- COMPLETED Box -->
+        <div class="container-wrapper">
             <h3 class="headerTitle">Completed</h3>
             <hr>
+            <div class="box-2"> <!-- Completed Box -->
 
-
-
+            </div>
         </div>
 
     </div>
