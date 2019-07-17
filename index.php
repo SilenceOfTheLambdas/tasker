3edd81f2e04e6ddf653189932008ec1822e03af8
Callum-James Smith <techdragon@tuta.io>
2019-07-17 22:48:51 +0100

    removed bottom info section and fixed layout

--- index.php
+++ index.php
@@ -37,6 +37,7 @@ if (isset($_SESSION['name']) && isset($_SESSION['id'])) // If the user already h
         <div id="top-logo">
             <img src="images/Tasker.io_Transparent.png">
         </div>
+
         <div id="buttons">
             <a href="#sign-in-modal" id="open-signin-modal"><button id='signin-button' type="submit" name="signin-submit">SIGN IN</button></a><br />
 
@@ -58,29 +59,8 @@ if (isset($_SESSION['name']) && isset($_SESSION['id'])) // If the user already h
         </div>
     </header>
 
-    <div id="info-box">
-        <h2>INFO</h2>
-        <hr>
-        <p>A free, no bull$h*t task management system.</p><br />
-        oh, and it's open source.
-    </div>
-
-    <main id="info">
-        <div>
-            <h2>Features</h2>
-            <ul>
-                <li>Easy to use</li>
-                <li>Customizable</li>
-                <li>Completely free</li>
-                <li>100% organic</li>
-            </ul>
-        </div>
-    </main>
-
-</body>
-
 <footer>
-    <pre style="color: black; text-align: center;">Callum-James Smith</pre>
+    <pre style="color: black; text-align: center;">Callum-James Smith | <a href="mailto:csmith99@protonmail.com">csmith99@protonmail.com</a></pre>
 </footer>
 <script>
     document.getElementById("open-signin-modal").onclick = function() // Stop the modal boxes from appearing quickly every time the page is refreshed
